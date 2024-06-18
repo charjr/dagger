e17d1e1dfe2e5f7433a2794edca60b1c3e64d859
John Charman <john.charman@imhotek.net>
2024-06-18 10:59:17 +0100

    Refactor DaggerObject ValueObject

--- sdk/php/tests/Unit/Fixture/NoDaggerFunctions.php
+++ sdk/php/tests/Unit/Fixture/NoDaggerFunctions.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Dagger\Tests\Unit\Fixture;
 
 use Dagger\Attribute\DaggerObject;
+use Dagger\ValueObject;
 
 #[DaggerObject]
 final class NoDaggerFunctions
@@ -12,5 +13,10 @@ final class NoDaggerFunctions
     public function notDaggerFunction(): bool
     {
         return true;
+    }
+
+    public static function getValueObjectEquivalent(): ValueObject\DaggerObject
+    {
+        return new ValueObject\DaggerObject(self::class, []);
     }
 }
