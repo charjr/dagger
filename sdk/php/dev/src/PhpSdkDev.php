1f68450d1a974dc062b2d1c18c62e7585c68183f
John Charman <john.charman@imhotek.net>
2024-08-09 15:29:20 +0100

    Add DaggerFunction "format" to PhpSdkDev
    Signed-off-by: John Charman <john.charman@imhotek.net>

--- sdk/php/dev/src/PhpSdkDev.php
+++ sdk/php/dev/src/PhpSdkDev.php
@@ -15,6 +15,8 @@ use function Dagger\dag;
 #[DaggerObject]
 final class PhpSdkDev
 {
+    private const SDK_ROOT='/src/sdk/php';
+
     #[DaggerFunction('Run tests from source directory')]
     public function test(
         #[Argument('Run tests from the given source directory')]
@@ -28,9 +30,51 @@ final class PhpSdkDev
         );
     }
 
-    #[DaggerFunction('Run linter in source directory')]
+    #[DaggerFunction('Lint the source directory')]
     public function lint(Directory $source): Container {
         return $this->base($source)->withExec(['phpcs']);
+    }
+
+    /**
+     * PHPCBF exit codes:
+     * 0: No errors were found
+     * 1: Errors were found, all errors were fixed
+     * 2: Errors were found, not all errors could be fixed
+     * 3: General script execution error occurred
+     *
+     * All but exit code 3 are successful executions of the formatter.
+     * These exit codes cannot be customised in configuration:
+     * https://github.com/squizlabs/PHP_CodeSniffer/issues/1818#issuecomment-354420927
+     *
+     * @TODO simplify this script if/when phpcbf can customise exit codes.
+     * This will most likely occur on a 4.0 release:
+     * https://github.com/PHPCSStandards/PHP_CodeSniffer/issues/184
+     */
+    #[DaggerFunction('Format the source directory')]
+    public function format(Directory $source): Directory
+    {
+        $scriptPath = '/usr/bin/format';
+        $script = <<<SH
+            #!/bin/sh
+
+            phpcbf
+
+            if [ $? != 3 ]
+            then
+                exit 0
+            fi
+
+            SH;
+
+        $original = $this->base($source)->directory(self::SDK_ROOT);
+
+        $formatted = $this->base($source)
+            ->withNewFile($scriptPath, $script)
+            ->withExec(['chmod', '+x', $scriptPath])
+            ->withExec([$scriptPath])
+            ->directory(self::SDK_ROOT);
+
+        return $original->diff($formatted);
     }
 
     private function base(Directory $source): Container
@@ -46,8 +90,8 @@ final class PhpSdkDev
                 ->cacheVolume('composer-php:8.3-cli-alpine'))
             ->withEnvVariable('COMPOSER_HOME', '/root/.composer')
             ->withEnvVariable('COMPOSER_ALLOW_SUPERUSER', '1')
-            ->withMountedDirectory('/src/sdk/php', $source)
-            ->withWorkdir('/src/sdk/php')
+            ->withMountedDirectory(self::SDK_ROOT, $source)
+            ->withWorkdir(self::SDK_ROOT)
             ->withExec(['composer', 'install'])
             ->withEnvVariable('PATH', './vendor/bin:$PATH', expand: true);
     }
