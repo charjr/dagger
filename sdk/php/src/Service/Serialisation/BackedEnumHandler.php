69b592774f2230e1e80dd516ae4e5b7d4ce0b29a
John Charman <john.charman@imhotek.net>
2024-09-06 18:45:15 +0100

    Add BackedEnum handling to Serialiser

---
+++ sdk/php/src/Service/Serialisation/BackedEnumHandler.php
@@ -0,0 +1,71 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Dagger\Service\Serialisation;
+
+use JMS\Serializer\Context;
+use JMS\Serializer\GraphNavigatorInterface;
+use JMS\Serializer\Handler\SubscribingHandlerInterface;
+use JMS\Serializer\JsonDeserializationVisitor;
+use JMS\Serializer\JsonSerializationVisitor;
+
+final readonly class BackedEnumHandler implements SubscribingHandlerInterface
+{
+    /**
+     * @return array<array{
+     *     direction: 1|2,
+     *     format: string,
+     *     type: string,
+     *     method: string,
+     * }>
+     */
+    public static function getSubscribingMethods(): array
+    {
+        return [
+            [
+                'direction' => GraphNavigatorInterface::DIRECTION_SERIALIZATION,
+                'format' => 'json',
+                'type' => \BackedEnum::class,
+                'method' => 'serialise',
+            ],
+            [
+                'direction' => GraphNavigatorInterface::DIRECTION_DESERIALIZATION,
+                'format' => 'json',
+                'type' => \BackedEnum::class,
+                'method' => 'deserialise'
+            ],
+        ];
+    }
+
+    public function serialise(
+        JsonSerializationVisitor $visitor,
+        \BackedEnum $enum,
+        array $type,
+        Context $context,
+    ): int|string {
+        return $enum->value;
+    }
+
+    public function deserialise(
+        JsonDeserializationVisitor $visitor,
+        int|string $enumValue,
+        array $type,
+        Context $context,
+    ): \BackedEnum {
+        $className = $type['params'][BackedEnumSubscriber::ORIGINAL_CLASS_NAME] ??
+            throw new \RuntimeException(
+                'Cannot find original class name.' .
+                ' If this issue occurs, it is a bug',
+            );
+
+        if (!in_array(\BackedEnum::class, class_implements($className) ?: [])) {
+            throw new \RuntimeException(
+                "'$className' was expected to be an enum." .
+                ' If this issue occurs, it is a bug',
+            );
+        }
+
+        return $className::from($enumValue);
+    }
+}
--- sdk/php/tests/Unit/Service/Serialisation/SerialiserTest.php
+++ sdk/php/tests/Unit/Service/Serialisation/SerialiserTest.php
@@ -1,15 +1,22 @@
 <?php
 
-namespace Dagger\tests\Unit\Service\Serialisation;
+namespace Dagger\Tests\Unit\Service\Serialisation;
 
 use Dagger\Client;
 use Dagger\ContainerId;
 use Dagger\Json;
+use Dagger\NetworkProtocol;
 use Dagger\Platform;
 use Dagger\Service\Serialisation\AbstractScalarHandler;
 use Dagger\Service\Serialisation\AbstractScalarSubscriber;
+use Dagger\Service\Serialisation\BackedEnumHandler;
+use Dagger\Service\Serialisation\BackedEnumSubscriber;
 use Dagger\Service\Serialisation\Serialiser;
+use Dagger\Tests\Unit\Fixture\DaggerObject\HandlingEnums;
+use Dagger\Tests\Unit\Fixture\Enum\StringBackedDummy;
 use Generator;
+use JMS\Serializer\EventDispatcher\EventSubscriberInterface;
+use JMS\Serializer\Handler\SubscribingHandlerInterface;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\Attributes\Group;
@@ -20,109 +27,140 @@ use PHPUnit\Framework\TestCase;
 #[CoversClass(Serialiser::class)]
 class SerialiserTest extends TestCase
 {
-    #[Test, DataProvider('provideScalars')]
-    public function itSerialisesScalars(mixed $value, string $valueAsJSON): void
-    {
-        self::assertSame($valueAsJSON, (new Serialiser())->serialise($value));
-    }
-
-    #[Test, DataProvider('provideScalars')]
-    public function itDeserialisesScalars(mixed $value, string $valueAsJSON): void
-    {
-        $sut = new Serialiser();
-
-        self::assertEquals($value, $sut->deserialise($valueAsJSON, gettype($value)));
-    }
-
-    #[Test, DataProvider('provideLists')]
-    public function itSerialisesLists(?array $value, string $valueAsJSON): void
-    {
-        self::assertSame($valueAsJSON, (new Serialiser())->serialise($value));
-    }
-
-    #[Test, DataProvider('provideLists')]
-    public function itDeserialisesLists(?array $value, string $valueAsJSON): void
-    {
-        $sut = new Serialiser();
-
-        self::assertEquals($value, $sut->deserialise($valueAsJSON, 'array'));
-    }
-
-    #[Test, DataProvider('provideAbstractScalars')]
-    public function itSerialisesAbstractScalars(
-        Client\AbstractScalar $value,
+    /**
+     * @param EventSubscriberInterface[] $subscribers
+     * @param SubscribingHandlerInterface[] $handlers
+     */
+    #[Test]
+    #[DataProvider('provideEnums')]
+    #[DataProvider('provideScalars')]
+    #[DataProvider('provideListsOfScalars')]
+    #[DataProvider('provideAbstractScalars')]
+    public function itSerialisesValues(
+        array $subscribers,
+        array $handlers,
+        mixed $value,
         string $valueAsJSON,
     ): void {
-        $sut = new Serialiser(
-            [new AbstractScalarSubscriber()],
-            [new AbstractScalarHandler()]
-        );
+        $sut = new Serialiser($subscribers, $handlers);
 
         self::assertSame($valueAsJSON, $sut->serialise($value));
     }
 
-    #[Test, DataProvider('provideAbstractScalars')]
-    public function itDeserialisesAbstractScalars(
-        Client\AbstractScalar $value,
+    /**
+     * @param EventSubscriberInterface[] $subscribers
+     * @param SubscribingHandlerInterface[] $handlers
+     */
+    #[Test]
+    #[DataProvider('provideEnums')]
+    #[DataProvider('provideScalars')]
+    #[DataProvider('provideListsOfScalars')]
+    #[DataProvider('provideAbstractScalars')]
+    public function itDeserialisesValues(
+        array $subscribers,
+        array $handlers,
+        mixed $value,
         string $valueAsJSON,
+        string $type,
     ): void {
-        $sut = new Serialiser(
-            [new AbstractScalarSubscriber()],
-            [new AbstractScalarHandler()]
-        );
+        $sut = new Serialiser($subscribers, $handlers);
 
-        self::assertEquals($value, $sut->deserialise($valueAsJSON, $value::class));
+        self::assertEquals($value, $sut->deserialise($valueAsJSON, $type));
     }
 
-    /** @return Generator<array{ 0: mixed, 1: string }> */
-    public static function provideScalars(): Generator
+        /**
+     * @return Generator<array{
+     *     0: array{ 0:BackedEnumSubscriber },
+     *     1: array{ 0:BackedEnumHandler },
+     *     2: mixed,
+     *     3: string,
+     *     4: string,
+     * }>
+     */
+    public static function provideEnums(): Generator
     {
         $cases = [
-            true,
-            false,
-            123,
-            null,
-            'expected',
-            'null',
+            [NetworkProtocol::TCP, '"TCP"'],
+            [StringBackedDummy::Hello, '"hello"'],
         ];
 
-        foreach ($cases as $case) {
-            $type = gettype($case);
-            yield "($type) $case" => [$case, json_encode($case)];
+        foreach ($cases as [$value, $valueAsJson]) {
+            yield $value::class => [
+                [new BackedEnumSubscriber()],
+                [new BackedEnumHandler()],
+                $value,
+                $valueAsJson,
+                $value::class,
+            ];
         }
     }
 
-    /** @return Generator<array{ 0: ?array, 1: string }> */
-    public static function provideLists(): Generator
+    /**
+     * @return Generator<array{
+     *     0: array{},
+     *     1: array{},
+     *     2: mixed,
+     *     3: string,
+     *     4: string,
+     * }>
+     */
+    public static function provideScalars(): Generator
     {
-        yield 'string[]' => [['hello', 'world'], '["hello","world"]'];
+        foreach ([true, false, 123, null, 'Hello, World!', 'null'] as $case) {
+            $type = gettype($case);
+            yield "($type) $case" => [[], [], $case, json_encode($case), $type];
+        }
 
-        yield 'null' => [null, 'null'];
-
-        yield ' null[]' => [[null, null], '[null,null]'];
+        yield 'expecting int, receive null' => [[], [], null, 'null', 'int'];
     }
 
     /**
-     * @return \Generator<array{
-     *     0: \Dagger\Client\AbstractScalar|null,
-     *     1: string,
+     * @return Generator<array{
+     *     0: array{},
+     *     1: array{},
+     *     2: mixed,
+     *     3: string,
+     *     4: string,
+     * }>
+     */
+    public static function provideListsOfScalars(): Generator
+    {
+        $cases = [
+            'string[]' => [['hello', 'world'], '["hello","world"]'],
+            'expecting array, receive null' => [null, 'null'],
+            'null[]' => [[null, null], '[null,null]'],
+        ];
+
+        foreach ($cases as $case => [$value, $valueAsJson]) {
+            yield $case => [[], [], $value, $valueAsJson, 'array'];
+        }
+    }
+
+    /**
+     * @return Generator<array{
+     *     0: array{ 0:AbstractScalarSubscriber },
+     *     1: array{ 0:AbstractScalarHandler },
+     *     2: mixed,
+     *     3: string,
+     *     4: string,
      * }>
      */
     public static function provideAbstractScalars(): Generator
     {
-        yield Platform::class => [
-            new Platform('linux/amd64'),
-            '"linux\/amd64"',
+        $cases = [
+            [new Platform('linux/amd64'), '"linux\/amd64"'],
+            [new Json('{"bool_field":true}'), '"{\"bool_field\":true}"'],
+            [new ContainerId('1234-567-89'), '"1234-567-89"'],
         ];
 
-        yield Json::class => [
-            new Json('{"bool_field":true}'),
-            '"{\"bool_field\":true}"',
-        ];
-
-        yield ContainerId::class => [
-            new ContainerId('1234-567-89'),
-            '"1234-567-89"',
-        ];
+        foreach ($cases as [$value, $valueAsJson]) {
+            yield $value::class => [
+                [new AbstractScalarSubscriber()],
+                [new AbstractScalarHandler()],
+                $value,
+                $valueAsJson,
+                $value::class,
+            ];
+        }
     }
 }
